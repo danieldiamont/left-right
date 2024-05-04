ee50002b2d8c701587e047cd4818cbde73e64d03
Daniel Diamont <daniel.diamont@gmail.com>
2024-05-04 17:03:05 -0500

    TestAddPlayers

--- main_test.go
+++ main_test.go
@@ -27,16 +27,35 @@ func closeConnTest(t *testing.T, c net.Conn) {
 
 func generateTestPlayer() Player {
     p := Player{}
-    p.ID = uuid.New()
+    p.ID, _ = uuid.Parse("0")
     return p
 }
 
 type Client struct {
     ID      uint16
     conn    net.Conn
+    enc     *gob.Encoder
+    dec     *gob.Decoder
+    t       *testing.T
 }
 
-func ClientSetup(network string, addr string) (*Client, error) {
+func (c *Client) Write(msg *Msg) {
+    err := c.enc.Encode(&msg)
+    if err != nil {
+        c.t.Fatalf("TEST - Failed to encode msg with error: %v\n", err)
+    }
+}
+
+func (c *Client) Read() Msg {
+    res := Msg{}
+    err := c.dec.Decode(&res)
+    if err != nil {
+        c.t.Fatalf("TEST - Failed to decode server response with error :%v\n", err)
+    }
+    return res
+}
+
+func ClientSetup(t *testing.T, network string, addr string) (*Client, error) {
 
     client := Client{}
     conn, err := net.Dial(network, addr)
@@ -44,6 +63,9 @@ func ClientSetup(network string, addr string) (*Client, error) {
         return nil, err
     }
     client.conn = conn
+    client.enc = gob.NewEncoder(client.conn)
+    client.dec = gob.NewDecoder(client.conn)
+
     return &client, nil
 }
 
@@ -58,6 +80,7 @@ func TestServerVersions(t *testing.T) {
     assert.Equal(t, 0, len(s.ConnPool), "Connection pool size should match")
     assert.Equal(t, version, s.GS.Version, "Versions should match")
     assert.Equal(t, 0, len(s.GS.Players), "Num Players should match")
+    assert.Equal(t, Idle, s.GS.State, "GS State should match")
 }
 
 func TestServerEcho(t *testing.T) {
@@ -66,7 +89,7 @@ func TestServerEcho(t *testing.T) {
     s := serverSetup(ver)
     defer s.stop()
     go s.run()
-    c, err := ClientSetup(TESTNET, TESTADDR)
+    c, err := ClientSetup(t, TESTNET, TESTADDR)
     if err != nil {
         t.Fatalf("TEST - Failed to create client with error: %v\n", err)
     }
@@ -75,23 +98,11 @@ func TestServerEcho(t *testing.T) {
     magic = 42
 
     defer closeConnTest(t, c.conn)
-    enc := gob.NewEncoder(c.conn)
-    dec := gob.NewDecoder(c.conn)
 
     msg := Msg{ Echo: true, Magic: magic, Player: generateTestPlayer()}
-
-    s.Logger.Info("TEST - Sending message", "msg", msg)
-
-    err = enc.Encode(&msg)
-    if err != nil {
-        t.Fatalf("TEST - Failed to encode msg with error: %v\n", err)
-    }
-
-    res := Msg{}
-    err = dec.Decode(&res)
-    if err != nil {
-        t.Fatalf("TEST - Failed to decode server response with error :%v\n", err)
-    }
+    c.Write(&msg)
+    
+    res := c.Read()
 
     assert.Equal(t, msg.Echo, res.Echo, "Echo field should match")
     assert.Equal(t, msg.Magic, res.Magic, "Magic field should match")
@@ -100,10 +111,42 @@ func TestServerEcho(t *testing.T) {
 }
 
 
-func TestAddPlayer(t *testing.T) {
+func TestAddPlayers(t *testing.T) {
+    numPlayers := 100
+
     var ver uint8
     ver = 1
-    _ = ver
+    s := serverSetup(ver)
+    defer s.stop()
+    go s.run()
 
+    clients := make([]*Client, 0)
+    for i := 0; i < numPlayers; i++ {
+        c, err := ClientSetup(t, TESTNET, TESTADDR)
+        if err != nil {
+            t.Fatalf("TEST - Failed to create client with error: %v\n", err)
+        }
+        defer closeConnTest(t, c.conn)
+        clients = append(clients, c)
+    }
+
+    for _, c := range clients {
+        msg := Msg{ Echo: false, Magic: uint16(3), Player: generateTestPlayer()}
+        c.Write(&msg)
+    }
+
+    for {
+        if len(clients) == len(s.GS.Players) {
+            break
+        }
+    }
+
+    assert.Equal(t, len(clients), len(s.ConnPool), "Number of clients should match size of connection pool")
+    assert.Equal(t, len(clients), len(s.GS.Players), "Number of clients should num players on server")
+
+    for _, p := range s.GS.Players {
+        assert.NotNil(t, p.ID, "Player ID should not be nil")
+        assert.NotEqual(t, uint32(0), p.ID.ID(), "Player ID should be non-zero")
+    }
 }
 
